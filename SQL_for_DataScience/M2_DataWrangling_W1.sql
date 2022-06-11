0d27d2a583b28da04e4b672d57bdefdc7d5c9714
Angelina Uesato <angelina.uesato@gmail.com>
2022-06-11 14:46:05 -0600

    Data_Formats Assigment
    Data_Formats Assigment SQL

--- SQL_for_DataScience/M2_DataWrangling_W1.sql
+++ SQL_for_DataScience/M2_DataWrangling_W1.sql
@@ -277,3 +277,31 @@ FROM
  FROM dsv1069.events
  WHERE
     event_name = 'view_user';
+
+-- Data Formats
+-- Exercise 1:
+-- Goal: Write a query to format the view_item event into a table with the appropriate columns
+SELECT
+  event_id,
+  event_time,
+  user_id,
+  platform,
+  --CAST(parameter_value AS INT ) AS viewed_user_id
+  MAX(CASE WHEN parameter_name = 'item_id'
+    THEN CAST(parameter_value AS INT )
+    ELSE NULL
+    END) AS item_id,
+    MAX(CASE WHEN parameter_name = 'referrer'
+    THEN parameter_value
+    ELSE NULL
+    END) AS referrer
+FROM
+  dsv1069.events
+WHERE
+  event_name = 'view_item'
+GROUP BY
+  event_id,
+  event_time,
+  user_id,
+  platform
+ORDER BY event_id
